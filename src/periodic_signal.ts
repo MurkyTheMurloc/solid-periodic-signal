05f1bb3cf0989da8497100789309c38b0ec978c3
Paul <72733450+paul1995tu@users.noreply.github.com>
2023-09-10 15:15:14 +0200

    fix type

--- src/periodic_signal.ts
+++ src/periodic_signal.ts
@@ -12,8 +12,8 @@ import { createSignal, from } from "solid-js";
  *   - startSignal: A function that can be used to start the periodic signal.
  *   - stopSignal: A function that can be used to stop the periodic signal.
  */
-export function createPeriodicSignal(updateFunction: () => void, periodInterval: number = 1 , inMs: boolean = false) {
-    const [id, setId] = createSignal<NodeJS.Timer>();
+export function createPeriodicSignal(updateFunction: () => void, periodInterval: number = 1 , inMs: boolean = false): [() => void, () => void] {
+    const [id, setId] = createSignal();
 
     const interval = inMs ? periodInterval : periodInterval * 1000;
 
